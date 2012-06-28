fe486085626b3219b4b891398d0704042c208bb3
Vladimir Shakhov <bogdad@gmail.com>
2012-06-28 13:12:21 +0400

    changed scripts

--- run-local.sh
+++ run-local.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-SRC_HOME = `pwd`
+SRC_HOME=`pwd`
 BIN_DIR="$SRC_HOME/src/main/bin"
 mkdir $SRC_HOME/target/tmp
 echo $BIN_DIR/java-with-classpath-local.sh  -Djava.io.tmpdir=$SRC_HOME/target/tmp ru.yandex.startrek.Main "$@"
