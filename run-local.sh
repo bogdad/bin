4fcb8d722edf521fa4e45e4e0a8448c79fc3efc4
Vladimir Shakhov <bogdad@gmail.com>
2012-06-28 13:13:30 +0400

    changed scripts

--- run-local.sh
+++ run-local.sh
@@ -1,8 +1,7 @@
 #!/bin/sh
 SRC_HOME=`pwd`
-BIN_DIR="$SRC_HOME/src/main/bin"
 mkdir $SRC_HOME/target/tmp
-echo $BIN_DIR/java-with-classpath-local.sh  -Djava.io.tmpdir=$SRC_HOME/target/tmp ru.yandex.startrek.Main "$@"
-exec $BIN_DIR/java-with-classpath-local.sh  -Djava.io.tmpdir=$SRC_HOME/target/tmp ru.yandex.startrek.Main "$@"
+java-with-classpath-local.sh  -Djava.io.tmpdir=$SRC_HOME/target/tmp ru.yandex.startrek.Main "$@"
+java-with-classpath-local.sh  -Djava.io.tmpdir=$SRC_HOME/target/tmp ru.yandex.startrek.Main "$@"
 
 # vim: set ts=4 sw=4 et:
