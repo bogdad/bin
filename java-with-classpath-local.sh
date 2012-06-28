b2e1a7ca5f2000bf45e5c8af266597b11b92d146
Vladimir Shakhov <bogdad@gmail.com>
2012-06-28 13:11:21 +0400

    changed scripts

--- java-with-classpath-local.sh
+++ java-with-classpath-local.sh
@@ -1,11 +1,11 @@
 #!/bin/sh -e
 
 export JAVA_HOME=/usr/local/java7
-
+SRC_HOME=`pwd`
 exec $JAVA_HOME/bin/java -server -showversion \
-    -classpath '/home/vshakhov/src/startrek/lib/bundles/*:/home/vshakhov/src/startrek/lib/jars/*:/home/vshakhov/src/startrek/target/jars:/home/vshakhov/src/startrek/target/classes:/home/vshakhov/src/startrek/src/main/java' \
+    -classpath '$SRC_HOME/lib/bundles/*:$SRC_HOME/lib/jars/*:$SRC_HOME/target/jars:$SRC_HOME/target/classes:$SRC_HOME/src/main/java' \
     -Dfile.encoding=UTF-8 \
-    -javaagent:/usr/lib/yandex/startrek/lib/org.aspectj-aspectjweaver-1.6.12.jar \
+    -javaagent:$SRC_HOME/lib/org.aspectj-aspectjweaver-1.6.12.jar \
     "$@"
 
 # vim: set ts=4 sw=4 et:
--- run-local.sh
+++ run-local.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
-
-BIN_DIR=`cd $(dirname $0); pwd`
-echo $BIN_DIR/java-with-classpath-local.sh  -Djava.io.tmpdir=/var/tmp ru.yandex.startrek.Main "$@"
-exec $BIN_DIR/java-with-classpath-local.sh  -Djava.io.tmpdir=/var/tmp ru.yandex.startrek.Main "$@"
+SRC_HOME = `pwd`
+BIN_DIR="$SRC_HOME/src/main/bin"
+mkdir $SRC_HOME/target/tmp
+echo $BIN_DIR/java-with-classpath-local.sh  -Djava.io.tmpdir=$SRC_HOME/target/tmp ru.yandex.startrek.Main "$@"
+exec $BIN_DIR/java-with-classpath-local.sh  -Djava.io.tmpdir=$SRC_HOME/target/tmp ru.yandex.startrek.Main "$@"
 
 # vim: set ts=4 sw=4 et:
